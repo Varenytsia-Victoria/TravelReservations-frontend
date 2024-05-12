32f39824a5fc7765401d607044ebae0ca8ac49ce
VarenytsiaV <victoriavarenytsia@gmail.com>
2024-05-12 21:30:45 +0300

    components added/home page design added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { MainComponent } from './main/main.component';
+import { HotelComponent } from './main/hotel/hotel.component';
+import { TripComponent } from './main/trip/trip.component';
+import { FlightComponent } from './main/flight/flight.component';
+import { CarHiveComponent } from './main/car-hive/car-hive.component';
+import { SortComponent } from './main/sort/sort.component';
+import { NavComponent } from './nav/nav.component';
+import { HotelCardComponent } from './main/hotel/hotel-card/hotel-card.component';
+import { TripCardComponent } from './main/trip/trip-card/trip-card.component';
+import { CarCardComponent } from './main/car-hive/car-card/car-card.component';
+import { FlightCardComponent } from './main/flight/flight-card/flight-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    UserComponent,
+    LoginComponent,
+    RegistrationComponent,
+    MainComponent,
+    HotelComponent,
+    TripComponent,
+    FlightComponent,
+    CarHiveComponent,
+    SortComponent,
+    NavComponent,
+    HotelCardComponent,
+    TripCardComponent,
+    CarCardComponent,
+    FlightCardComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/main/car-hive/car-hive.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-hive',
+  templateUrl: './car-hive.component.html',
+  styleUrls: ['./car-hive.component.scss']
+})
+export class CarHiveComponent {
+
+}
